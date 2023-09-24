b6481241a1aaf7777599c3833dd900ae44e95933
bhupeshlahare <bhupeshlahare@gmail.com>
2023-09-24 20:59:29 +0530

    Repository Interface

---
+++ Book.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Book.DataAccess.Data;
+using Book.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDBContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+            //_db.Categories == dbSet
+        }
+
+        void IRepository<T>.Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        T IRepository<T>.Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        void IRepository<T>.Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        void IRepository<T>.RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
